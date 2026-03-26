c59f15dff30c37fa51f75b29f5e0567d5aad80dd
PC <112487@ecole-it.com>
2026-03-26 14:34:21 +0100

    python1

---
+++ ex21.py
@@ -0,0 +1,5 @@
+list=[3,7,3,1,3,8,3]
+nb=list.count(3)
+print(nb)
+pos=list.index(8)
+print(pos)
---
+++ ex22.py
@@ -0,0 +1,6 @@
+list=[1,2,3]
+list1=[4,5,6]
+list.extend(list1)
+print(list)
+list.append(list1)
+print(list)
